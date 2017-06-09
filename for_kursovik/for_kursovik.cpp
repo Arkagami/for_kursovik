4e0648608928653433eea1c0a49b5e5974359980
Arkagami <mixno20@gmail.com>
2017-06-09 21:14:50 +0300

    1

--- for_kursovik/for_kursovik.cpp
+++ for_kursovik/for_kursovik.cpp
@@ -1,11 +1,38 @@
-// for_kursovik.cpp: определяет точку входа для консольного приложения.
-//
-
-#include "stdafx.h"
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
 
 
 int main()
 {
+	char s[1000][1000];
+
+	FILE *fin = fopen("in.mixno", "r");
+	FILE *fout = fopen("out.mixno", "w");
+	int ir = 0;
+
+	while (!feof(fin)) {
+		fgets(s[ir++], 999, fin);
+	}
+	fclose(fin);
+
+	for (int i = 0;i < 24;i++) {
+		for (int k = 0;k < ir;k++) {
+			for (int l = 0;l < strlen(s[k]);l++) {
+				if (s[k][l] == '#') {
+					fprintf(fout, "%d", i + 1);
+				} else
+				if (s[k][l] == '@') {
+					fprintf(fout, "%d", i);
+				}
+				else
+					fputc(s[k][l], fout);
+			}
+		}
+		fputc('\n', fout);
+	}
+	
+	fclose(fout);
     return 0;
 }
 
